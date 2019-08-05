4146d3fe01c2a85f9774004f9153fba8ed09d837
PacersCyc <452720855@qq.com>
2019-08-05 23:00:19 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send(
+    `<html>
+      <head>title</head>
+      <body>
+        <h1>first lesson</h1>
+        <p>hello world<p>
+      </body>
+    </html>`
+  )
+});
+
+var server = app.listen(3000);
