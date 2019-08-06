0639bd498dc3ff58c208b06c633ec4e749a6e421
PacersCyc <452720855@qq.com>
2019-08-06 20:03:43 +0800

    webpack server config

---
+++ src/containers/Home/index.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+const React = require('react');
+
+const Home = () => (
+  <div>home</div>
+)
+
+// export default Home
+module.exports = {
+  default: Home
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const Home = require('./containers/Home');
 
 app.get('/', function(req, res) {
   res.send(
     `<html>
-      <head>title</head>
+      <head>
+        <title>hello</title>
+      </head>
       <body>
         <h1>first lesson</h1>
         <p>hello world<p>
