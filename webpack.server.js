294a824456029d854255025a63211e186c302b2e
PacersCyc <452720855@qq.com>
2019-08-07 00:07:26 +0800

    优化目录结构

--- webpack.server.js
+++ webpack.server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
+const serverConfig = {
   mode: 'development',
   target: 'node',
-  entry: './src/index.js',
+  entry: './src/server/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
@@ -12,27 +14,6 @@ module.exports = {
   externals: [
     nodeExternals()
   ],
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            [
-              'env',
-              {
-                targets: {
-                  browsers: 'last 2 versions'
-                }
-              }
-            ]
-          ]
-        }
-      }
-    ]
-  }
 }
+
+module.exports = merge(baseConfig, serverConfig);
