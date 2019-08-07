61a92832ebb73eff970a0683182c0c21ef596eed
PacersCyc <452720855@qq.com>
2019-08-07 21:48:00 +0800

    store change

--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
+import getStore from '../store'
 
 import Routes from '../Routes'
 
-const reducer = (state = {name: 'cyc'}, action) => {
-  return state;
-}
-
-const store = createStore(reducer, applyMiddleware(thunk));
-
 const App = () => {
   return (
-    <Provider store={store}>
+    <Provider store={getStore()}>
       <BrowserRouter>
         {Routes}
       </BrowserRouter>
