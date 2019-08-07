16fa2826fab9cd1fa039d2598675e3e92f955895
PacersCyc <452720855@qq.com>
2019-08-07 20:35:51 +0800

    redux init

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import Routes from '../Routes'
 
+const reducer = (state = {name: 'cyc'}, action) => {
+  return state;
+}
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 const App = () => {
   return (
-    <BrowserRouter>
-      {Routes}
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        {Routes}
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { connect } from 'react-redux'
 
-const Home = () => (
+const Home = (props) => (
   <div>
     <Header />
-    <div>hello world</div>
+    <div>this is {props.name}</div>
     <button onClick={() => {
       alert('click')
     }}>click</button>  
   </div>
 )
 
-export default Home
+const mapStateToProps = state => ({
+  name: state.name
+})
+
+export default connect(mapStateToProps, null)(Home);
