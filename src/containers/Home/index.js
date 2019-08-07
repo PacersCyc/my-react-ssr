13516d4bee0d3efa47b859d8c0d13436f87e88c1
PacersCyc <452720855@qq.com>
2019-08-07 22:57:18 +0800

    redux样板代码初始化

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../../components/Header';
 import { connect } from 'react-redux'
+import { getHomeList } from './store/actions'
 
-const Home = (props) => (
-  <div>
-    <Header />
-    <div>this is {props.name}</div>
-    <button onClick={() => {
-      alert('click')
-    }}>click</button>  
-  </div>
-)
+class Home extends Component {
+  componentDidMount() {
+    this.props.getHomeList()
+  }
+
+  renderList() {
+    const { list } = this.props;
+    return list.map((item, index) => (
+      <div key={index}>
+        <a href={item.link} target="_blank">{item.title}</a>
+      </div>
+    ))
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.renderList()}
+        <button onClick={() => {
+          alert('click')
+        }}>click</button>  
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
-  name: state.name
+  list: state.home.newsList
 })
 
-export default connect(mapStateToProps, null)(Home);
+const mapDispatchToProps = dispatch => ({
+  getHomeList() {
+    dispatch(getHomeList());
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ src/containers/Home/store/actions.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { CHANGE_LIST } from './constants'
+
+const url = 'http://47.95.113.63/ssr/api/news.json'
+const url2 = 'https://3g.163.com/touch/reconstruct/article/list/BBM54PGAwangning/0-10.html'
+const url3 = 'https://www.apiopen.top/journalismApi'
+
+const changeList = (list) => ({
+  type: CHANGE_LIST,
+  list
+})
+
+export const getHomeList = () => {
+  return (dispatch) => {
+    axios.get(url3).then(res => {
+      console.log(res);
+      dispatch(changeList(res.data.data.sports))
+    })
+  }
+} 
